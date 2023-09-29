ad14c43f488de7e5f436f4ef1688bfc4278cab6c
elissandroa <elissandroa@hotmail.com>
2023-09-29 09:42:00 -0300

    Seeding database

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -12,4 +12,15 @@ INSERT INTO tb_aluno (nome,cpf,bairro,data_de_nascimento) values ('Selena','9234
 INSERT INTO tb_aluno (nome,cpf,bairro,data_de_nascimento) values ('Carla','103456789','Bairro 10','1990-08-15');
 			
 	            
-INSERT INTO tb_avaliacao_fisica (altura_atual,peso_atual,aluno_id,data_da_avaliacao) VALUES (1.75,47.5,1,'2023-09-28');	                      										 
+INSERT INTO tb_avaliacao_fisica (altura_atual,peso_atual,aluno_id,data_da_avaliacao) VALUES (1.75,47.5,1,'2023-09-05');	   
+INSERT INTO tb_avaliacao_fisica (altura_atual,peso_atual,aluno_id,data_da_avaliacao) VALUES (1.75,47.2,1,'2023-09-12');
+INSERT INTO tb_avaliacao_fisica (altura_atual,peso_atual,aluno_id,data_da_avaliacao) VALUES (1.75,45.0,1,'2023-09-19');	 
+INSERT INTO tb_avaliacao_fisica (altura_atual,peso_atual,aluno_id,data_da_avaliacao) VALUES (1.75,44.7,1,'2023-09-26');	     
+
+
+INSERT INTO tb_matricula (data_da_matricula,aluno_id) values (now(),1);  	
+INSERT INTO tb_matricula (data_da_matricula,aluno_id) values (now(),2);  	  
+INSERT INTO tb_matricula (data_da_matricula,aluno_id) values (now(),3);  	  
+INSERT INTO tb_matricula (data_da_matricula,aluno_id) values (now(),4);  	  
+INSERT INTO tb_matricula (data_da_matricula,aluno_id) values (now(),5);  	  
+INSERT INTO tb_matricula (data_da_matricula,aluno_id) values (now(),6);  	                         										 
